fb1ad4e3fa6e8c079e782d82c3fb6a45e3905f09
Des Albert <des.albert@gmail.com>
2026-03-13 10:26:35 -0700

    update

--- src/main/kotlin/org/dba/nvltest/MainController.kt
+++ src/main/kotlin/org/dba/nvltest/MainController.kt
@@ -51,7 +51,7 @@ private val targetFileConfig = FileParseConfig(
 )
 
 
-private val NVL72_GB300 = "NVL72 GB300 Configurator v16.0.xlsx"
+private val NVL72_GB300 = "NVL72 GB300 Configurator v17.0.xlsx"
 private val NVL72_GB200 = "NVL72 GB200 Configurator v7.0.xlsx"
 private val NVL4_GB200 = "NVL4 GB200 Configurator v8.0.xlsx"
 private val NVL72_VR200 = "NVL72 VR200 Configurator v3.0.xlsx"
